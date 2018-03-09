ce29828e419fe6cdc5abbfa2dbfaf02ad73f2e98
sergeysviridenko <sergey.v.sviridenko@gmail.com>
2018-03-09 18:49:56 +0200

    s

---
+++ src/Modules/Information/Module.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Developer Tools                                                |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-present Phalcon Team (https://www.phalconphp.com)   |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>             |
+  +------------------------------------------------------------------------+
+*/
+
+namespace Phalcon\Devtools\Modules\Information;
+
+use Phalcon\Loader;
+use Phalcon\DiInterface;
+use Phalcon\Devtools\Modules\Core\Module\AbstractModule;
+
+/**
+ * Phalcon\Devtools\Modules\Information\Module
+ *
+ * Class to export config data to user application
+ *
+ * @package Phalcon\Devtools\Modules\Information
+ */
+class Module extends AbstractModule
+{
+    /**
+     * Register a specific autoloader for the module
+     */
+    public function registerAutoloaders(DiInterface $di = null)
+    {
+        $this->registerDir();
+        $this->registerRoute();
+    }
+
+    public function registerServices(DiInterface $di)
+    {
+
+    }
+
+    protected function registerDir()
+    {
+        (new Loader)->registerDirs(
+            [
+                dirname(__FILE__) . '/Controllers',
+            ]
+        )
+            ->register();
+    }
+
+    protected function registerRoute()
+    {
+
+    }
+}
