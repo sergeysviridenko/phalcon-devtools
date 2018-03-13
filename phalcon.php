f5ac2f35072be461871d426fffd5afc75bc949e1
sergeysviridenko <sergey.v.sviridenko@gmail.com>
2018-03-13 18:17:13 +0200

    Add base data

--- phalcon.php
+++ phalcon.php
@@ -5,7 +5,7 @@
   +------------------------------------------------------------------------+
   | Phalcon Developer Tools                                                |
   +------------------------------------------------------------------------+
-  | Copyright (c) 2011-2016 Phalcon Team (https://www.phalconphp.com)      |
+  | Copyright (c) 2011-present Phalcon Team (https://www.phalconphp.com)   |
   +------------------------------------------------------------------------+
   | This source file is subject to the New BSD License that is bundled     |
   | with this package in the file LICENSE.txt.                             |
@@ -14,77 +14,36 @@
   | obtain it through the world-wide-web, please send an email             |
   | to license@phalconphp.com so we can send you a copy immediately.       |
   +------------------------------------------------------------------------+
-  | Authors: Andres Gutierrez <andres@phalconphp.com>                      |
-  |          Eduar Carvajal <eduar@phalconphp.com>                         |
-  |          Serghei Iakovlev <serghei@phalconphp.com>                     |
+  | Authors: Sergii Svyrydenko <sergey.v.sviridenko@gmail.com              |
   +------------------------------------------------------------------------+
 */
 
-use Phalcon\Script;
-use Phalcon\Script\Color;
-use Phalcon\Devtools\Version;
-use Phalcon\Commands\Builtin\Info;
-use Phalcon\Commands\Builtin\Model;
-use Phalcon\Commands\Builtin\Module;
-use Phalcon\Commands\Builtin\Project;
-use Phalcon\Commands\Builtin\Scaffold;
-use Phalcon\Commands\CommandsListener;
-use Phalcon\Commands\Builtin\Webtools;
-use Phalcon\Commands\Builtin\AllModels;
-use Phalcon\Commands\Builtin\Migration;
-use Phalcon\Commands\Builtin\Enumerate;
-use Phalcon\Commands\Builtin\Controller;
-use Phalcon\Commands\Builtin\Serve;
-use Phalcon\Commands\Builtin\Console;
+use Devtools\Application;
+use Devtools\Core\Devtools\Version;
+use Devtools\Core\String\StringColorize;
 use Phalcon\Exception as PhalconException;
-use Phalcon\Commands\DotPhalconMissingException;
-use Phalcon\Events\Manager as EventsManager;
 
 try {
     require dirname(__FILE__) . '/bootstrap/autoload.php';
-
     $vendor = sprintf('Phalcon DevTools (%s)', Version::get());
-    print PHP_EOL . Color::colorize($vendor, Color::FG_GREEN, Color::AT_BOLD) . PHP_EOL . PHP_EOL;
+    print PHP_EOL . StringColorize::colorize($vendor, StringColorize::FG_GREEN, StringColorize::AT_BOLD) . PHP_EOL . PHP_EOL;
 
-    $eventsManager = new EventsManager();
+    $applicarion = new Application();
+    $applicarion->run($argv);
 
-    $eventsManager->attach('command', new CommandsListener());
+    $result = true;
+} catch (\Exception $e) {
+    fwrite(STDERR, StringColorize::info($e->getMessage() . " " . $e->getMessage()));
 
-    $script = new Script($eventsManager);
-
-    $commandsToEnable = [
-        Info::class,
-        Enumerate::class,
-        Controller::class,
-        Module::class,
-        Model::class,
-        AllModels::class,
-        Project::class,
-        Scaffold::class,
-        Migration::class,
-        Webtools::class,
-        Serve::class,
-        Console::class,
-    ];
-
-    $script->loadUserScripts();
-
-    foreach ($commandsToEnable as $command) {
-        $script->attach(new $command($script, $eventsManager));
-    }
-
-    $script->run();
-} catch (DotPhalconMissingException $e) {
-    fwrite(STDERR, Color::info($e->getMessage() . " " . $e->scanPathMessage()));
-    if ($e->promptResolution()) {
-        $script->run();
-    } else {
-        exit(1);
-    }
+    $result = false;
 } catch (PhalconException $e) {
-    fwrite(STDERR, Color::error($e->getMessage()) . PHP_EOL);
-    exit(1);
-} catch (Exception $e) {
-    fwrite(STDERR, 'ERROR: ' . $e->getMessage() . PHP_EOL);
-    exit(1);
+    fwrite(STDERR, StringColorize::info($e->getMessage() . " " . $e->getMessage()));
+
+    $result = false;
+} catch (\Throwable $e) {
+    fwrite(STDERR, StringColorize::info($e->getMessage() . " " . $e->getMessage()));
+
+    $result = false;
 }
+
+$result || exit(1);
--- webtools.php
+++ webtools.php
@@ -4,7 +4,7 @@
   +------------------------------------------------------------------------+
   | Phalcon Developer Tools                                                |
   +------------------------------------------------------------------------+
-  | Copyright (c) 2011-2016 Phalcon Team (https://www.phalconphp.com)      |
+  | Copyright (c) 2011-present Phalcon Team (https://www.phalconphp.com)   |
   +------------------------------------------------------------------------+
   | This source file is subject to the New BSD License that is bundled     |
   | with this package in the file LICENSE.txt.                             |
@@ -18,20 +18,3 @@
   |          Serghei Iakovlev <serghei@phalconphp.com>                     |
   +------------------------------------------------------------------------+
 */
-
-use Phalcon\Bootstrap;
-
-include 'webtools.config.php';
-include PTOOLSPATH . '/bootstrap/autoload.php';
-
-$bootstrap = new Bootstrap([
-    'ptools_path' => PTOOLSPATH,
-    'ptools_ip'   => PTOOLS_IP,
-    'base_path'   => BASE_PATH,
-]);
-
-if (APPLICATION_ENV === ENV_TESTING) {
-    return $bootstrap->run();
-} else {
-    echo $bootstrap->run();
-}
